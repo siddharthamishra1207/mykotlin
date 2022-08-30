0e57f700e28822d8799af456b21292dcaa4aa3a0
siddharthamishra1207 <mishrasiddhartha2001@gmail.com>
2022-08-30 22:12:31 +0530

    updated till functions

--- ap/src/main/java/com/example/ap/MyClass.kt
+++ ap/src/main/java/com/example/ap/MyClass.kt
@@ -1,7 +1,88 @@
 package com.example.ap
 
-fun main(){
-    var name  = "siddhartha"
+fun main() {
 
-    println("hello $name")
+    // Part-1
+    // var - mutable (can be change )
+    //val - immutable
+    val n: String // kotlin is like expacit type
+    n = "sidd"
+
+    val age: Int
+    age = 18
+    var name = "siddhartha"
+    name = "mishra"
+    // println("hello name is $name and age is $age")
+
+    //part-2
+    /*
+    what is bit?
+      -smallest unit of data or information
+    Int - 32 bits
+    Byte - 8 bits
+    Shorts - 16 bits
+    Long - 64 bits
+
+    float and Double
+    Float- decimal no. upto 32 bits
+    Double- upto 64 bits
+     */
+
+    //Part -3
+    /*
+    kotlin operation
+    +
+    -
+    *
+    /
+    %
+
+     */
+
+//    val a = 12.0
+//    val b = 5.0
+//    println(a/b)
+
+    //Part-4 (looping and Branch)
+
+    val a1 = 1
+//    if(a1==10){
+//        println("equal to 10")
+//    }
+//    else{
+//        println("not equale to")
+//    }
+
+//    when (a1) {
+//        in 1..10 -> println("no. is between 1 to 10")
+//        !in 1..10 -> println("not in 1 to 100")
+//       // 10 -> println(" you have 10")
+//        100 -> println(" you have 100")
+//        1000 -> println(" you have 1000")
+//        else -> {
+//            println("$a1 amount is")
+//        }
+//
+//    }
+
+    //for loop
+//    for (i in 1..100){
+//        if(i%2==0){
+//            println(i)
+//        }
+//    }
+  calc(2,5)
+    calc(67,89090)
+    PrintString(3,"error 404",9)
+}
+
+fun calc(a:Int,b:Int){
+    println(a*b)
+}
+
+fun PrintString(a:Int,b:String,c:Int){
+
+    for (i in a..c){
+       println(b)
+    }
 }
